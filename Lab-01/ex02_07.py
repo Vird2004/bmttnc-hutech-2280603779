f4b874a4e256c9ed74090af514477b8a600661a2
grangerlangdon <reddragon.vird04@gmail.com>
2025-02-28 10:00:32 +0700

    update ex02_07

---
+++ Lab-01/ex02_07.py
@@ -0,0 +1,12 @@
+#Nhap du lieu tu nguoi dung
+print("Nhap cac dong van ban (Nhap 'done' de ket thuc): ")
+lines = []
+while True:
+    line = input()
+    if line.lower() == 'done':
+        break
+    lines.append(line)
+#Chuyen cac dong thanh chu hoa roi in ra man hinh
+print("\n Cac dong da nhap sau khi chuyen thanh chu in hoa: ")
+for line in lines:
+    print(line.upper())    
