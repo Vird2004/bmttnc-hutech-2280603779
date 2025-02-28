5f07cc6bba503ce45eac0f4c12ca0eae1152323c
grangerlangdon <reddragon.vird04@gmail.com>
2025-02-28 10:38:32 +0700

    update

---
+++ Lab-01/ex02/ex02_01.py
@@ -0,0 +1,6 @@
+#nhap ten nguoi dung
+ten = input("Nhap ten nguoi dung: ")
+tuoi = input("Nhap tuoi nguoi dung: ")
+
+#In ra te, tuoi nguoi dung
+print ("Welcome, ", ten, "! Ban", tuoi, "tuoi." )
---
+++ Lab-01/ex02/ex02_05.py
@@ -0,0 +1,7 @@
+so_gio_lam = float(input("Nhap so gio lam moi tuan: "))
+luong_gio = float(input("Nhap thu lao tren moi gio theo tieu chuan: "))
+gio_tieu_chuan = 44 # So gio tieu chuan moi tuan
+gio_vuot_chuan = max(0, so_gio_lam - gio_tieu_chuan) #so gio lam vuot chuan
+thuc_linh = gio_tieu_chuan*luong_gio + gio_vuot_chuan*luong_gio*1.5 #Tong thu nhap
+
+print(f"So tien thuc linh cua nhan vien: {thuc_linh}")
